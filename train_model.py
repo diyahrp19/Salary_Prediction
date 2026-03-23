1f1136044c31ded220fcb96b37a6c1c99ff7844d
Diya Patel <diyait05@gmail.com>
2026-03-23 14:05:27 +0530

    Done

---
+++ train_model.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import pickle
+
+data = {
+    "experience": [1,2,3,4,5],
+    "salary": [20000,35000,40000,90000,100000]
+}
+
+df = pd.DataFrame(data)
+
+x = df[['experience']]
+y = df[['salary']]
+
+model = LinearRegression()
+model.fit(x, y)
+
+pickle.dump(model, open('model.pkl', 'wb'))
+
+print("Model trained & saved")
